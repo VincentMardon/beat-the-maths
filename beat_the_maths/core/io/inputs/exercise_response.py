f61e7553398b65e3626dfdcdd83c26157f8908bf
Vincent Mardon <mardon.vincent@gmail.com>
2025-10-29 00:49:58 +0100

    fix(ui): replace French prompt with English ("How much is ...  ? ")

--- beat_the_maths/core/io/inputs/exercise_response.py
+++ beat_the_maths/core/io/inputs/exercise_response.py
@@ -2,6 +2,6 @@ import time
 
 def input_response(problem: int, exercise_number: int) -> str:
     start_time = time.time()
-    response = input(f"{exercise_number}. Combien font {problem}").strip()
+    response = input(f"{exercise_number}. How much is {problem}").strip()
     duration = time.time() - start_time
     return response, duration
